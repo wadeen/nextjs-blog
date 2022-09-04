e74f5cd63ae99c2d36c3ceee496c7bde4b749744
wadeen <meister.vvdo@gmail.com>
2022-09-04 19:42:06 +0900

    update: _documentsファイルでGoogle fontsを管理

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,19 @@ import { seoType } from 'types/seoType'
 const MyDocument: NextPage = () => {
   return (
     <Html>
-      <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#" />
+      <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+        {/* Nikukyu (Japanese): https://fonts.google.com/earlyaccess Google fonts: https://fonts.google.com/*/}
+        <link
+          href="https://fonts.googleapis.com/earlyaccess/nikukyu.css"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Capriola&family=Noto+Sans+JP:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
