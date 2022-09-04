10e98ee7aae88f092c87674b569e89ee7851e1fc
wadeen <meister.vvdo@gmail.com>
2022-09-04 15:05:58 +0900

    update: strage準備中の旨掲載

--- src/pages/strage.tsx
+++ src/pages/strage.tsx
@@ -4,9 +4,8 @@ import Seo from 'src/components/Seo'
 const Strage: NextPage = () => {
   return (
     <>
-      <Seo ogpTitle="自己紹介 | Webのあれこれ" />
-      ようこそ！ App倉庫で、実験や開発をしたものを置いています。
-      コメントなどあればお願いします。
+      <Seo ogpTitle="App倉庫 | Webのあれこれ" />
+      <p>【現在準備中】学習中に作成したWebアプリを掲載していきます。</p>
     </>
   )
 }
