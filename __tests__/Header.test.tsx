7ac062a3ac385df6e7dab23c315df1db4f042f00
wadeen <meister.vvdo@gmail.com>
2022-10-16 10:51:29 +0900

    add: ヘッダーのテスト追記中

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -1,15 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event'
-import { getPage, initTestHelpers } from 'next-page-tester';
+import '@testing-library/jest-dom/extend-expect'
+// import { initTestHelpers, getPage } from 'next-page-tester'
 
 // initTestHelpers()
 
-describe('Header navigation by  Link', () => {
-  it('Should route to selected page in Header menu', async () => {
-    const { page } = await getPage({
-      route: '/index',
-    })
+describe('ヘッダーメニューのリンク遷移確認', () => {
+  it('メインメニューをクリック', async () => {
+    // const { page } = await getPage({
+    //   route: '/index',
+    // })
     // render(page)
 
     // userEvent.click(screen.getByTestId('blog-nav'))
