b97265a3b01771b49c25474aa8b3afa82628f1b9
wadeen <meister.vvdo@gmail.com>
2022-12-13 16:10:36 +0900

    refactoring: swrカテゴリ数の取得(途中)

--- src/components/organisms/aside/AsideCategory.tsx
+++ src/components/organisms/aside/AsideCategory.tsx
@@ -1,51 +1,72 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-// import axios from 'axios'
 import Link from 'next/link'
+import { useEffect } from 'react'
 import useSWR from 'swr'
-import { client } from '../../../../libs/client'
-import { MicrocmsData } from '../../../../types/microcmsData'
+import { client } from 'libs/client'
 import AsideTitle from 'src/components/atoms/aside/AsideTitle'
+import { microCmsPostData } from 'types/microCmsPostData'
+import { MicrocmsData } from 'types/microcmsData'
+
+// type Category = {
+//   category: string
+//   totalCount: string
+// }[]
 
 const AsideCategory = () => {
+  // let categoryData: any = []
+
   // カテゴリ一覧の取得
 
   const categoriesFetch = async () => {
-    const category = await client.get({
-      endpoint: 'categories',
-    })
-    return category
+    // 全てのカテゴリを取得
+    const categories = await client.get({ endpoint: 'categories' })
+    return categories
+
+    // カテゴリ別の記事を取得
+    // const categoryPostCategoryData = categories.contents.map(
+    //   async (category: microCmsPostData) => {
+    //     await client
+    //       .get({
+    //         endpoint: 'posts',
+    //         queries: {
+    //           filters: `category[equals]${category.id}`,
+    //           limit: 999,
+    //         },
+    //       })
+    //       // .then((res) => console.log('res: ', res.contents))
+    //       .then((res) => {
+    //         categoryData.push(
+    //           {
+    //           // category: {
+    //           category: res.contents[0].category.id,
+    //           totalCount: res.totalCount,
+    //           // },
+    //         }
+    //         )
+    //       })
+    //       .catch((err) => console.log(err))
+    //   }
+    // )
+    // console.log('categoryData: ', categoryData)
+    // return categoryPostCategoryData
   }
 
-  // ページ件数の取得
-  // const categoriesNumber = async () => {
-  //   const categories = await client.get({ endpoint: 'categories' })
+  let hoge: string[] = []
 
-  //   for (const category of categories.contents) {
-  //     const categoryContents = await client.get({
-  //       endpoint: 'posts',
-  //       queries: {
-  //         filters: `category[equals]${category.id}`,
-  //       },
-  //     })
-  //     console.log(category.id)
-  //   }
-  // }
-
-  const { data, error } = useSWR('category', categoriesFetch)
+  const { data, error } = useSWR('categories', categoriesFetch)
   if (error) console.log(error.message)
 
   // カテゴリの取得結果の判定
   return data ? (
     <>
-      <AsideTitle text={'Category'} />
       <ul css={categoryList}>
+        <AsideTitle text={'Category'} />
         {data.contents.map((categoryPage: MicrocmsData) => (
           <li key={categoryPage.id}>
-            {/* 最初は1ページに遷移する */}
             <Link href={`/category/${categoryPage.id}`}>
               {categoryPage.name}
-              {/* <span>({categoryPage.length})</span> */}
+              {/* <span>{categoryTotalCount}</span> */}
             </Link>
           </li>
         ))}
