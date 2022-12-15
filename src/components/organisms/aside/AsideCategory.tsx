e98feedf903ce1b8a8696f55a826771ac6840da2
wadeen <meister.vvdo@gmail.com>
2022-12-15 21:30:36 +0900

    info: カテゴリIDとカテゴリ1件取得

--- src/components/organisms/aside/AsideCategory.tsx
+++ src/components/organisms/aside/AsideCategory.tsx
@@ -21,59 +21,55 @@ const AsideCategory = () => {
   const categoriesFetch = async () => {
     // 全てのカテゴリを取得
     const categories = await client.get({ endpoint: 'categories' })
-    return categories
+    // return categories
+    // const categoryId = categories.contents[0].id
 
-    // カテゴリ別の記事を取得
-    // const categoryPostCategoryData = categories.contents.map(
-    //   async (category: microCmsPostData) => {
-    //     await client
-    //       .get({
-    //         endpoint: 'posts',
-    //         queries: {
-    //           filters: `category[equals]${category.id}`,
-    //           limit: 999,
-    //         },
-    //       })
-    //       // .then((res) => console.log('res: ', res.contents))
-    //       .then((res) => {
-    //         categoryData.push(
-    //           {
-    //           // category: {
-    //           category: res.contents[0].category.id,
-    //           totalCount: res.totalCount,
-    //           // },
-    //         }
-    //         )
-    //       })
-    //       .catch((err) => console.log(err))
-    //   }
-    // )
-    // console.log('categoryData: ', categoryData)
-    // return categoryPostCategoryData
+    const categoryPost = await client.get({
+      endpoint: 'posts',
+      queries: {
+        filters: `category[equals]react`,
+        limit: 999,
+      },
+    })
+    return { categories, categoryPost }
   }
 
-  let hoge: string[] = []
+  // categoriesFetch()
+  // console.log(categoryPost)
+
+  // カテゴリ別の記事を取得
+  // const categoryPostCategoryData = categories.contents.map(
+  //   async (category: microCmsPostData) => {
+  //     await client
+  //       .get({
+  //         endpoint: 'posts',
+  //         queries: {
+  //           filters: `category[equals]${category.id}`,
+  //           limit: 999,
+  //         },
+  //       })
+  //       .catch((err) => console.log(err))
+  //   }
+  // )
 
   const { data, error } = useSWR('categories', categoriesFetch)
   if (error) console.log(error.message)
 
+  console.log('data: ', data)
+
   // カテゴリの取得結果の判定
-  return data ? (
-    <>
-      <ul css={categoryList}>
-        <AsideTitle text={'Category'} />
-        {data.contents.map((categoryPage: MicrocmsData) => (
-          <li key={categoryPage.id}>
-            <Link href={`/category/${categoryPage.id}`}>
-              {categoryPage.name}
-              {/* <span>{categoryTotalCount}</span> */}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  ) : (
-    <></>
+  return (
+    <ul css={categoryList}>
+      <AsideTitle text={'Category'} />
+      {data.contents.map((categoryPage: MicrocmsData) => (
+        <li key={categoryPage.id}>
+          <Link href={`/category/${categoryPage.id}`}>
+            {categoryPage.name}
+            {/* <span>{categoryTotalCount}</span> */}
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
