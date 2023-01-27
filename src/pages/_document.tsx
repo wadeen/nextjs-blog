c4def6df0ac3de58b46cf696b080164930bbe64f
wadeen <meister.vvdo@gmail.com>
2023-01-27 21:25:58 +0900

    fix: manifestのパスの変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,8 @@ const MyDocument: NextPage = () => {
   return (
     <Html lang="ja">
       <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
-        <link rel="manifest" href="/manifest/manifest.json" />
-        <link rel="apple-touch-icon" href="/manifest/icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#fff" />
       </Head>
       <body>
