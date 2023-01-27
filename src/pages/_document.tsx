a6b3bdaede0e5f7a8461c06dbeb60c90c95b7b70
wadeen <meister.vvdo@gmail.com>
2023-01-27 21:21:46 +0900

    add: pws必要なファイルをインストール

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const MyDocument: NextPage = () => {
   return (
     <Html lang="ja">
-      <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#"></Head>
+      <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+        <link rel="manifest" href="/manifest/manifest.json" />
+        <link rel="apple-touch-icon" href="/manifest/icon.png" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
