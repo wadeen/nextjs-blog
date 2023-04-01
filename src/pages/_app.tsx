2e97cfb9484ccb428dae0705aa6befd457e26471
wadeen <meister.vvdo@gmail.com>
2023-04-02 00:47:23 +0900

    Modify: ディスクリプションの見直しとアナリティクスの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
 import '../styles/globals.css'
 import '../styles/destyle.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 import Layout from '../components/templates/Layout'
+// @ts-ignore
+import * as gtag from '/src/utils/gtag'
+import { GaScript } from 'src/utils/GaScript'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <RecoilRoot>
       <Layout>
+        <GaScript />
         <Component {...pageProps} />
       </Layout>
     </RecoilRoot>
